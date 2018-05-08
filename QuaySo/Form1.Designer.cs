2d7144f004a3cec9d4eb33ba857ceb90890551ea
Si Ta <Si Ta>
2018-05-08 10:01:51 +0700

    Add project files.

---
+++ QuaySo/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace QuaySo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.picBox3 = new System.Windows.Forms.PictureBox();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Quay/Dung";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picBox3
+            // 
+            this.picBox3.Location = new System.Drawing.Point(514, 77);
+            this.picBox3.Name = "picBox3";
+            this.picBox3.Size = new System.Drawing.Size(88, 138);
+            this.picBox3.TabIndex = 1;
+            this.picBox3.TabStop = false;
+            // 
+            // picBox2
+            // 
+            this.picBox2.Location = new System.Drawing.Point(377, 77);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(88, 138);
+            this.picBox2.TabIndex = 2;
+            this.picBox2.TabStop = false;
+            // 
+            // picBox1
+            // 
+            this.picBox1.Location = new System.Drawing.Point(235, 77);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(88, 138);
+            this.picBox1.TabIndex = 3;
+            this.picBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 504);
+            this.Controls.Add(this.picBox1);
+            this.Controls.Add(this.picBox2);
+            this.Controls.Add(this.picBox3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picBox3;
+        private System.Windows.Forms.PictureBox picBox2;
+        private System.Windows.Forms.PictureBox picBox1;
+    }
+}
+
---
+++ QuaySo/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuaySo
+{
+    public partial class Form1 : Form
+    {
+        string[] strImage;
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        public void Init()
+        {
+            picBox1.BackgroundImage = Image.FromFile(@"Images/so0.png");
+            picBox2.BackgroundImage = Image.FromFile(@"Images/so0.png");
+            picBox3.BackgroundImage = Image.FromFile(@"Images/so0.png");
+            strImage = new string[]
+            {
+                "so0.png","so1.png","so2.png","so3.png","so4.png","so5.png","so6.png","so7.png","so8.png","so9.png"
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int milliseconds = 200;
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(milliseconds);
+                picBox1.BackgroundImage = Image.FromFile(@"Images/" + strImage[i]);
+                picBox2.BackgroundImage = Image.FromFile(@"Images/" + strImage[i]);
+                picBox3.BackgroundImage = Image.FromFile(@"Images/" + strImage[i]);
+            }
+        }
+    }
+}
